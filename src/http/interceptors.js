c7747acf9b5c2a12c6f0dce8069763e95ed046a1
osdeni <osdeni@gmail.com>
2019-12-31 18:30:46 -0300

    tratamento geral de 403

--- src/http/interceptors.js
+++ src/http/interceptors.js
@@ -5,6 +5,13 @@ export default req => {
         if (status === 401) {
             store.dispatch('auth/ActionSignOut');
             window._Vue.$router.push({ name: 'login' });
+        } else if (status === 403) {
+            window._Vue.$toast.open({
+                position: "top",
+                message: "Você não possui permissão para realizar esta operação",
+                type: "error"
+              });
+            window._Vue.$router.go(-1);
         }
     }
 };
