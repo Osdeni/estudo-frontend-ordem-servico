171b5939adcfbf161128a116dd20fce3e1509eb8
osdeni <osdeni@gmail.com>
2020-01-02 17:55:39 -0300

    Ajuste do componete cliente para: pessoa

--- src/http/services.js
+++ src/http/services.js
@@ -2,7 +2,7 @@ import { services as auth } from '@/modules/auth';
 import { services as ordemServico } from '@/pages/ordemServico';
 import { services as tipo } from '@/pages/tipo';
 import { services as marca } from '@/pages/marca';
-import { services as cliente } from '@/pages/cliente';
+import { services as cliente } from '@/pages/pessoa';
 
 export default {
     auth,
